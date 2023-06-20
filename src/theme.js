2920f7deae7984131b2b9a1776a2ffdac65be8de
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-06-20 20:01:06 +0400

    add theming

---
+++ src/theme.js
@@ -0,0 +1,28 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: 'rgb(255, 192, 31)'
+        }
+    },
+    components: {
+        MuiButton: {
+            defaultProps: {
+                disableRipple: true
+            },
+            styleOverrides: {
+                root: {
+                    borderRadius: 20,
+                    textTransform: 'capitalize',
+                    color: 'black',
+                    borderColor: 'black',
+                    "&:hover": {
+                        backgroundColor: 'rgb(255, 192, 31)'
+                    }
+                }
+            }
+        }
+    }
+})
+export default theme;
