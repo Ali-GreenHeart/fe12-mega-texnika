f8aefb3c46a98d25b9160c73411dcf6010caa4e5
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-06-20 20:52:18 +0400

    add PaperImg compo, new section

---
+++ src/components/PaperImg/index.jsx
@@ -0,0 +1,14 @@
+import { Paper } from "@mui/material"
+
+const PaperImg = ({ src }) => {
+    return (
+        <Paper sx={{ p: 2 }}>
+            <img
+                style={{ width: '100%' }}
+                src={src}
+                alt="eskavator"
+            />
+        </Paper>
+    )
+}
+export default PaperImg
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,9 +1,12 @@
-import { Button, Container, Grid, Paper, Typography } from "@mui/material"
+import { Box, Button, Container, Grid, Paper, Typography } from "@mui/material"
 import PageContainer from "../../components/PageContainer"
 import ekskavator from "../../assets/ekskavator_1.png"
 import ekskavator_yukleyici from "../../assets/ekskavator_yukleyici.png"
 import eskavator from "../../assets/eskavator.png"
+import forklift from "../../assets/forklift.png"
+import belas from "../../assets/belas.png"
 import CategoryCard from "../../components/CategoryCard"
+import PaperImg from "../../components/PaperImg"
 
 const texnikalar = [
     {
@@ -59,13 +62,7 @@ const Home = () => {
                         <Button variant="outlined">Ətraflı</Button>
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <Paper sx={{ p: 2 }}>
-                            <img
-                                style={{ width: '100%' }}
-                                src={ekskavator}
-                                alt="eskavator"
-                            />
-                        </Paper>
+                        <PaperImg src={ekskavator} />
                     </Grid>
                 </Grid>
                 <Typography my={2} variant="h6" fontWeight="bold">Kateqoriyalar</Typography>
@@ -80,6 +77,19 @@ const Home = () => {
                             </Grid>
                         })
                     }
+                </Grid>
+                <Grid container alignItems="flex-end" justifyContent="space-between">
+                    <Grid item md={4} lg={3} sx={{ display: { xs: 'none', md: 'block' } }}>
+                        <PaperImg src={forklift} />
+                    </Grid>
+                    <Grid item xs={12} md={7} lg={8} >
+                        <PaperImg src={belas} />
+                        <Box pl={5} pt={5} >
+                            <Typography variant="h4" fontWeight="bold">Mega Texnika-da sınaqdan keçirilmiş texnikalar</Typography>
+                            <Typography my={2}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled</Typography>
+                            <Button variant="outlined">Haqqımızda</Button>
+                        </Box>
+                    </Grid>
                 </Grid>
             </Container>
         </PageContainer>
