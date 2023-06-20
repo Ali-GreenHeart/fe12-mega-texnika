76b8e798be4a3723d55403dbc18b748f9b7f7710
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-06-20 20:35:54 +0400

    category card customization

--- src/components/CategoryCard/index.jsx
+++ src/components/CategoryCard/index.jsx
@@ -2,7 +2,16 @@ import { Paper, Typography } from "@mui/material"
 
 const CategoryCard = ({ title, src }) => {
     return (
-        <Paper elevation={16} sx={{ maxWidth: '300px' }}>
+        <Paper elevation={16}
+            sx={{
+                maxWidth: '300px',
+                height: 300,
+                margin: '0 auto',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'space-between',
+                p: 2
+            }}>
             <Typography pt={4} component="h4" fontWeight="bold" textAlign="center">{title}</Typography>
             <img style={{ width: '100%', objectFit: 'contain' }} src={src} />
         </Paper>
