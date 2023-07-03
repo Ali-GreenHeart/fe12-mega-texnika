8258d477444d219d2224d2fa396d7a3b96e9e021
IdrisVeli1987 <idrisveliev@gmail.com>
2023-07-03 22:38:44 +0400

    fix issues

--- src/components/LeaseModalError/index.jsx
+++ src/components/LeaseModalError/index.jsx
@@ -1,6 +1,7 @@
-import { Box, Container, Dialog, Typography } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import PaperImg from "../PaperImg";
 import error from "../../assets/LeaseModalError/error.png";
+import Texnikalar from "../../pages/Texnikalar";
 
 let obj = {
   title: "Sifarişiniz tamamlanmadı",
@@ -9,11 +10,39 @@ let obj = {
 
 const LeaseModalError = () => {
   return (
-      <Box sx={{ p: 2, }} >
-        <PaperImg src={error} sx={{with: 200}} />
-        <Typography variant="h4" className="firstTitle">{obj.title}</Typography>
-        <Typography variant="h6" className="secontTitle">{obj.text}</Typography>
-      </Box>
+    <Box sx={{ p: 2 }}>
+      <Grid
+        xs={12}
+        sm={6}
+        md={2}
+        item
+        className="error_img"
+        sx={{ p: 2, width: 170 }}
+        style={{ margin: "0 auto" }}
+      >
+        <PaperImg src={error} className="error" />
+      </Grid>
+      <Typography
+        variant="h4"
+        className="firstTitle"
+        p={2}
+        xs={12}
+        sm={6}
+        md={3}
+      >
+        {obj.title}
+      </Typography>
+      <Typography
+        variant="h6"
+        className="secontTitle"
+        p={2}
+        xs={12}
+        sm={6}
+        md={3}
+      >
+        {obj.text}
+      </Typography>
+    </Box>
   );
 };
 export default LeaseModalError;
