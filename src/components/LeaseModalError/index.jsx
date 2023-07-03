3669d4ca38032d51e1bcc3263c12cca7bf0e8eb2
IdrisVeli1987 <idrisveliev@gmail.com>
2023-07-03 14:46:53 +0400

    add LeaseModalError Component

---
+++ src/components/LeaseModalError/index.jsx
@@ -0,0 +1,10 @@
+import { Dialog } from "@mui/material";
+
+const LeaseModalError = () => {
+    return(
+        <Dialog>
+            
+        </Dialog>
+    )
+};
+export default LeaseModalError
