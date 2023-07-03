4073f7c52543863a3e97e0382045ca570a9f830b
IdrisVeli1987 <idrisveliev@gmail.com>
2023-07-03 16:47:43 +0400

    fix issue

--- src/components/LeaseLoading/index.jsx
+++ src/components/LeaseLoading/index.jsx
@@ -1,6 +1,11 @@
-const LeaseLoading = () => {
-    return (
-        <div className="lease_loading_on_modal"></div>
-    )
-}
-export default LeaseLoading
+import { useEffect } from "react";
+
+const LeaseLoading = ({ setStep }) => {
+  useEffect(() => {
+    setTimeout(() => {
+      setStep(2);
+    }, 1000);
+  }, []);
+  return <div className="lease_loading_on_modal"></div>;
+};
+export default LeaseLoading;
