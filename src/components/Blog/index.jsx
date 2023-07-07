d9100b7befa8895a7c0646cbba25d7d0dade69c5
Ali-GreenHeart <elinemet.isiyev@gmail.com>
2023-07-07 21:23:43 +0400

    add blog pagination

--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Grid, Paper, Stack, Typography } from "@mui/material";
+import { Box, Container, Grid, Pagination, Paper, Stack, Typography } from "@mui/material";
 import PageContainer from "../../components/PageContainer";
 import Traktor from "../../assets/traktor.png";
 import news1 from "../../assets/news1.png"
@@ -7,6 +7,7 @@ import news3 from "../../assets/news3.png"
 import CategoryCard from "../CategoryCard";
 import PaperImg from "../PaperImg";
 import BlogCard from "../BlogCard";
+import { useState } from "react";
 
 
 const news = [
@@ -57,11 +58,108 @@ const news = [
     date: '20.12.2023',
     title: "Xeberin adi",
     img: news2
+  },
+  {
+    id: 10,
+    date: '20.12.2023',
+    title: "Xeberin adi0",
+    img: news1
+  },
+  {
+    id: 11,
+    date: '20.12.2023',
+    title: "Xeberin adi1",
+    img: news2
+  },
+  {
+    id: 12,
+    date: '20.12.2023',
+    title: "Xeberin adi2",
+    img: news3
+  },
+  {
+    id: 13,
+    date: '20.12.2023',
+    title: "Xeberin adi3",
+    img: news1
+  },
+  {
+    id: 14,
+    date: '20.12.2023',
+    title: "Xeberin adi4",
+    img: news2
+  },
+  {
+    id: 15,
+    date: '20.12.2023',
+    title: "Xeberin adi5",
+    img: news3
+  },
+  {
+    id: 16,
+    date: '20.12.2023',
+    title: "Xeberin adi6",
+    img: news1
+  },
+  {
+    id: 17,
+    date: '20.12.2023',
+    title: "Xeberin adi7",
+    img: news2
+  },
+  {
+    id: 20,
+    date: '20.12.2023',
+    title: "Xeberin adi20",
+    img: news1
+  },
+  {
+    id: 21,
+    date: '20.12.2023',
+    title: "Xeberin adi21",
+    img: news2
+  },
+  {
+    id: 22,
+    date: '20.12.2023',
+    title: "Xeberin adi22",
+    img: news3
+  },
+  {
+    id: 23,
+    date: '20.12.2023',
+    title: "Xeberin adi23",
+    img: news1
+  },
+  {
+    id: 24,
+    date: '20.12.2023',
+    title: "Xeberin adi24",
+    img: news2
+  },
+  {
+    id: 25,
+    date: '20.12.2023',
+    title: "Xeberin adi25",
+    img: news3
+  },
+  {
+    id: 26,
+    date: '20.12.2023',
+    title: "Xeberin adi26",
+    img: news1
+  },
+  {
+    id: 27,
+    date: '20.12.2023',
+    title: "Xeberin adi27",
+    img: news2
   }
 ]
 
 
 const Blog = () => {
+  const [page, setPage] = useState(1)
   return (
     <PageContainer>
       <Container className="line_raduis">
@@ -87,7 +185,7 @@ const Blog = () => {
         </Grid>
         <Grid container spacing={2} justifyContent="center" py={2} my={4}>
           {
-            news.map(({ id, title, img, date }) => {
+            news.slice((page - 1) * 8, (page - 1) * 8 + 8).map(({ id, title, img, date }) => {
               return <Grid component="a" href={`/blog/${id}`} key={id} item xs={12} sm={6} md={4} lg={3}>
                 <BlogCard
                   title={title}
@@ -98,6 +196,9 @@ const Blog = () => {
             })
           }
         </Grid>
+        <Stack alignItems="center">
+          <Pagination onChange={(e, value) => setPage(value)} count={5} color="primary" />
+        </Stack>
       </Container>
     </PageContainer>
   );
